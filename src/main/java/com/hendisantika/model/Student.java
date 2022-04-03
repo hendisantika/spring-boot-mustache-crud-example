d53028a450e185d1e5b7294ebcaf2b6c2340da53
Hendi Santika <hendisantika@gmail.com>
2022-04-03 15:43:20 +0700

    #6 - Add Student Mode

---
+++ src/main/java/com/hendisantika/model/Student.java
@@ -0,0 +1,40 @@
+package com.hendisantika.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mustache-crud-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/04/22
+ * Time: 15.42
+ */
+@Data
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "jurusan")
+    private String jurusan;
+
+}
